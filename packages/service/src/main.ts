e0580778e9927b146b67019c2a7c228ea88fcbae
xlcodes <xiaolin201809@126.com>
2024-07-09 23:12:50 +0800

    feat: 添加响应拦截器、日志收集器

--- packages/service/src/main.ts
+++ packages/service/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ConfigService} from "@nestjs/config";
 import {ValidationPipe} from "@nestjs/common";
+import {FormatResponseInterceptor} from "./common/interceptor/format-response.interceptor";
+import {InvokeRecordInterceptor} from "./common/interceptor/invoke-record.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,8 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalInterceptors(new FormatResponseInterceptor())
+  app.useGlobalInterceptors(new InvokeRecordInterceptor())
 
   const port = configService.get<number>('APPLICATION_PORT') || 3000;
 
@@ -16,4 +20,5 @@ async function bootstrap() {
     console.log(`服务已经启动：http://localhost:${port}`)
   });
 }
+
 bootstrap();
